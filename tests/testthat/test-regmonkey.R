cf3c01a229dacd6d80e3dc507bfd89ce61bb2c05
gohuntsuan <gohuntsuan@gmail.com>
2024-08-28 16:37:12 +0800

    add unit test

---
+++ tests/testthat/test-regmonkey.R
@@ -0,0 +1,23 @@
+test_that("regmonkey function output matches expected results", {
+  df <- mtcars
+  results <- regmonkey(
+    data = df,
+    dependent_var = "mpg",
+    independent_vars = c("cyl"),
+    control_vars = c("am"),
+    significance_level = 0.1
+  )
+
+  expect_results <- list(
+    list(
+      independent_var = "cyl",
+      controls = character(0)
+    ),
+    list(
+      independent_var = "cyl",
+      controls = c("am")
+    )
+  )
+
+  expect_equal(results, expect_results)
+})
